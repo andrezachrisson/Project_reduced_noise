57230ca4c2c825bc65481842ed7327ad66fe449b
andrezachrisson <andrezachrisson@gmail.com>
2019-01-03 13:45:27 +0100

    Final comments

--- src/Main.py
+++ src/Main.py
@@ -20,27 +20,22 @@ import glob
 
 def fastPhylo(normal_file, tempdir):
         """ Reduced noise tree"""
-        with open(tempdir+'/red_fast', 'w') as file: #open in tempdir
-            run(['fastprot','reduced_file'], stdin=PIPE, stdout=file, stderr=PIPE, encoding = 'utf8', cwd=tempdir) #cdw = tempdir
+        with open(tempdir+'/red_fast', 'w') as file:
+            run(['fastprot','reduced_file'], stdin=PIPE, stdout=file, stderr=PIPE, encoding = 'utf8', cwd=tempdir)
 
-            # file.write(process.stdout.read())
-
-        with open(tempdir+'/red_tree', 'w') as file:  #open in tempdir
-            run(['fnj','red_fast', '-O', 'newick'], stdin=PIPE, stdout=file, stderr=PIPE, encoding = 'utf8', cwd=tempdir) #cdw = tempdir
-            # file.write(process.stdout.read())
+        with open(tempdir+'/red_tree', 'w') as file:
+            run(['fnj','red_fast', '-O', 'newick'], stdin=PIPE, stdout=file, stderr=PIPE, encoding = 'utf8', cwd=tempdir)
 
         """ Normal tree"""
-        with open(tempdir+'/normal_fast', 'w') as file:  #open in tempdir
+        with open(tempdir+'/normal_fast', 'w') as file:
             run(['fastprot', normal_file], stdin=PIPE, stdout=file, stderr=PIPE, encoding = 'utf8')
-            # file.write(process.stdout.read())
 
-        with open(tempdir+'/normal_tree', 'w') as file: #open in tempdir
-            run(['fnj','normal_fast', '-O', 'newick'], stdin=PIPE, stdout=file, stderr=PIPE, encoding = 'utf8', cwd=tempdir) #cdw = tempdir
-            # file.write(process.stdout.read())
+        with open(tempdir+'/normal_tree', 'w') as file:
+            run(['fnj','normal_fast', '-O', 'newick'], stdin=PIPE, stdout=file, stderr=PIPE, encoding = 'utf8', cwd=tempdir)
+
         return tree_compare (tempdir)
 
 def tree_compare(tempdir):
-        # CHANGE to tempdir
         tns = dendropy.TaxonNamespace()
         tree1 = Tree.get_from_path(
                 tempdir+"/ref.tree",
@@ -64,34 +59,37 @@ def tree_compare(tempdir):
 def main():
     os.chdir('..')
     dirpath = os.path.dirname(os.path.realpath(__file__))
-    # most recent edited folder
     directory = dirpath + '/data'
+    # Choose the most recent data folder
     directory = max(glob.glob(os.path.join(directory, '*/')), key=os.path.getmtime)
-
     all_testdata =[]
+
     for data in os.listdir(directory):
         data = directory +'/' + data
         os.chdir(data)
+        # Create a temporary directory where we will work
         with tempfile.TemporaryDirectory(dir = data) as tempdir:
+            # Find and copy the reference tree to temporary directory
             for filename in os.listdir(data):
                 if os.path.isfile(filename):
-                    #print(filename[0])
                     if filename[0] != '.':
                         if re.search(r'tree', filename):
                             copyfile(filename,tempdir+'/ref.tree')
                             statistics =[(filename,filename+'_reduced')]
                             break
 
-
             for filename in os.listdir(data):
                 if os.path.isfile(filename):
                     if filename[0] != '.' and not re.search(r'tree', filename):
+                            # Create the reduced file
                             if read_file(filename, tempdir):
+                                # Run the subprocesses
                                 statistics.append(fastPhylo(filename, tempdir))
                             else:
                                 pass
         all_testdata.append(statistics)
     localtime = time.localtime(time.time())
+    # Save results in results folder
     os.chdir(dirpath + '/results')
     today = str(localtime.tm_year)+'_'+str(localtime.tm_mon)+'_'+str(localtime.tm_mday)
     if not os.path.exists(today):
